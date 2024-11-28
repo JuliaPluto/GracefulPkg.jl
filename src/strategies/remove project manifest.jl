a08e58be746dbfad427693c3493a1c8bccba221b
Fons van der Plas <fonsvdplas@gmail.com>
2024-11-28 19:27:41 +0100

    Create remove project manifest.jl

---
+++ src/strategies/remove project manifest.jl
@@ -0,0 +1,39 @@
+struct StrategyRemoveManifest <: Strategy end
+
+action_text(::StrategyRemoveManifest) = "Removing Manifest.toml file"
+
+
+
+condition(::StrategyRemoveManifest, ctx::StrategyContext) = isfile(manifest_file(ctx))
+
+
+
+function action(::StrategyRemoveManifest, ctx::StrategyContext)
+    m = manifest_file(ctx)
+    isfile(m) && rm(m)
+end
+
+
+
+
+
+##############
+
+
+
+struct StrategyRemoveProject <: Strategy end
+
+action_text(::StrategyRemoveProject) = "Removing Project.toml and Manifest.toml files"
+
+
+condition(::StrategyRemoveProject, ctx::StrategyContext) = isfile(manifest_file(ctx)) || isfile(project_file(ctx))
+
+function action(::StrategyRemoveProject, ctx::StrategyContext)
+    m = manifest_file(ctx)
+    p = manifest_file(ctx)
+    isfile(m) && rm(m)
+    isfile(p) && rm(p)
+end
+
+
+
