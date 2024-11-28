0c67edcfbd5b6497510615348c107ce6f2a54b11
Fons van der Plas <fonsvdplas@gmail.com>
2024-11-28 19:27:36 +0100

    Create fix stdlibs.jl

---
+++ src/strategies/fix stdlibs.jl
@@ -0,0 +1,17 @@
+
+struct StrategyFixStdlibs <: Strategy end
+
+action_text(::StrategyFixStdlibs) = "Fixing stdlib dependencies"
+
+
+
+function condition(::StrategyFixStdlibs, ctx::StrategyContext)
+
+    error()
+end
+
+
+
+function action(::StrategyFixStdlibs, ctx::StrategyContext)
+    error()
+end
