fb0f0c76d8c04fd5544e55c91932df75c449c9bc
Fons van der Plas <fonsvdplas@gmail.com>
2024-11-28 19:27:49 +0100

    Create fixture helpers.jl

---
+++ test/fixture helpers.jl
@@ -0,0 +1,20 @@
+
+import GracefulPkg
+import Pkg
+
+
+
+function go(original_dir)
+
+    dir = tempname()
+
+    cp(original_dir, dir)
+
+
+    @assert isdir(dir)
+    @assert !isempty(readdir(dir))
+
+    result = Pkg.activate(dir) do
+        GracefulPkg.resolve()
+    end
+end
