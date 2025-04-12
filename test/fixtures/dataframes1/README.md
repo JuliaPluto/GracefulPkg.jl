a5fa5cc0cfc58ddf7f534f8c24ac98a30e6d7f47
Fons van der Plas <fonsvdplas@gmail.com>
2025-04-13 00:07:34 +0200

    add dataframes fixture

---
+++ test/fixtures/dataframes1/README.md
@@ -0,0 +1,4 @@
+https://github.com/fonsp/package-loading-times/blob/0ae2fd7af7e3ac5f081303493c54ffea5e7d6c86/most_downloaded_packages.jl
+
+
+from https://github.com/fonsp/Pluto.jl/issues/2642#issuecomment-1773742351
---
+++ test/fixtures/dataframes1/test.jl
@@ -0,0 +1,5 @@
+result = go(@__DIR__)
+@test test_max_severity(result, GracefulPkg.StrategyRemoveManifest)
+strats = [r.strategy for r in result.strategy_reports]
+
+@test test_max_severity(result, GracefulPkg.StrategyFixStdlibs)
