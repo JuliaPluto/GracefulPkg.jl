cc6fe4db6c8902a21dc1c3c1d727b78b54257c6d
Fons van der Plas <fonsvdplas@gmail.com>
2025-04-12 19:04:49 +0200

    Update test.jl

--- test/fixtures/future version/test.jl
+++ test/fixtures/future version/test.jl
@@ -2,8 +2,7 @@ result = go(@__DIR__)
 strats = [r.strategy for r in result.strategy_reports]
 
 
-
-@test GracefulPkg.StrategyDoNothing() ∈ strats
 @test GracefulPkg.StrategyLoosenCompat() ∈ strats
+@test GracefulPkg.StrategyUpdateRegistry() ∈ strats
 
 @test last(strats) == GracefulPkg.StrategyLoosenCompat()
