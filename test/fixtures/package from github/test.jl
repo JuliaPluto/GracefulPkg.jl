8bc24259f041ee6a4b6c5384b617e9de7915de30
Fons van der Plas <fonsvdplas@gmail.com>
2025-05-27 10:29:30 +0200

    yehhh

--- test/fixtures/package from github/test.jl
+++ test/fixtures/package from github/test.jl
@@ -1,14 +1,14 @@
 result = go(@__DIR__)
-@test test_max_severity(result, GracefulPkg.StrategyLoosenCompat)
+@test test_max_severity(result, GracefulPkg.StrategyInstantiateIfNeeded)
 strats = [r.strategy for r in result.strategy_reports]
 
 @test GracefulPkg.StrategyDoNothing() in strats
-@test GracefulPkg.StrategyLoosenCompat() in strats
+@test GracefulPkg.StrategyInstantiateIfNeeded() in strats
 
 fixed_it = last(result.strategy_reports)
 
-@test fixed_it.strategy isa GracefulPkg.StrategyLoosenCompat
+@test fixed_it.strategy isa GracefulPkg.StrategyInstantiateIfNeeded
 
-@test fixed_it.project_changed
-@test fixed_it.manifest_changed
+@test !fixed_it.project_changed
+@test !fixed_it.manifest_changed
 @test !fixed_it.registry_changed
