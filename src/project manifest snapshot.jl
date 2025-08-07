a35ad017f2832e70dabecfd4ffe0892092c36eaa
Fons van der Plas <fonsvdplas@gmail.com>
2025-08-07 16:42:37 +0200

    Mark ProjectManifestSnapshot, take_project_manifest_snapshot as public API

--- src/project manifest snapshot.jl
+++ src/project manifest snapshot.jl
@@ -1,12 +1,23 @@
+using Compat
+
+@compat public ProjectManifestSnapshot, take_project_manifest_snapshot
 
 """
-The contents of the Project.toml and Manifest.toml files at the time of recording.
+The contents of the Project.toml and Manifest.toml files at the time of recording. `nothing` if a file does not exist.
 """
 Base.@kwdef struct ProjectManifestSnapshot
     project::Union{Nothing,String} = nothing
     manifest::Union{Nothing,String} = nothing
 end
 
+
+"""
+```julia
+take_project_manifest_snapshot(env_dir::String)::ProjectManifestSnapshot
+```
+
+Take a snapshot of the Project.toml and Manifest.toml files in the given environment directory. Returns a [`ProjectManifestSnapshot`](@ref) object.
+"""
 function take_project_manifest_snapshot(env_dir::String)
     p = joinpath(env_dir, "Project.toml")
     m = joinpath(env_dir, "Manifest.toml")
