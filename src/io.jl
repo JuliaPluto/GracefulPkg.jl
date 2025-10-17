558e407364aefdd0ef94c3b55aae22437fd0a72e
Fons van der Plas <fonsvdplas@gmail.com>
2025-10-17 16:14:58 +0200

    Update io.jl

--- src/io.jl
+++ src/io.jl
@@ -1,7 +1,7 @@
 
 
 function Base.showerror(io::IO, nw::GracefulPkg.NothingWorked)
-    printstyled(io, "GracefulPkg.NothingWorked: Nothing worked...\n"; bold=true)
+    printstyled(io, "GracefulPkg.NothingWorked: The task failed, and no strategies from GracefulPkg could fix it...\n"; bold=true)
     
     report = nw.report
     
@@ -18,14 +18,25 @@ function Base.show(io::IO, ::MIME"text/plain", report::GracefulPkg.GraceReport)
     
     printstyled(io, "=== BEGIN GRACEFULPKG REPORT ===\n"; color=:light_black)
     
-    if !isempty(reps)
+    if length(reps) == 1 && is_success(report) && reps[1].strategy isa StrategyDoNothing
+        printstyled(io, "The task was successfully completed without any actions.\n"; color=:green)
+    elseif !isempty(reps)
         actions = unique!([action_text_not_empty(r.strategy) for r in reps])
         
         println(io, "I tried the following actions:")
         for action in actions
             println(io, " - $(action)")
         end
+        if is_success(report)
+            printstyled(io, "And the last one worked!\n"; color=:green)
+        else
+            print(io, "But in the end, ")
+            printstyled(io, "nothing worked :( \n"; color=:light_red, bold=true)
+            println(io, "Here is more information:")
+        end
+        
         println(io)
+        
     
         
         last_shown_exception = Ref("asdf")
@@ -33,13 +44,13 @@ function Base.show(io::IO, ::MIME"text/plain", report::GracefulPkg.GraceReport)
         for r in reps
             e_str = sprint(showerror, r.exception; context=io)
             printstyled(io, "-----------\n"; color=:light_black)
-            printstyled(io, "Applying strategy: $(action_text_not_empty(r.strategy))...\n"; bold=true)
+            printstyled(io, "I applied the strategy: $(action_text_not_empty(r.strategy))...\n"; bold=true)
             # println(io)
-            if r.success
-                printstyled(io, "Task successfully completed.\n"; color=:green)
+            if is_success(r)
+                printstyled(io, "✔ Task successfully completed.\n"; color=:light_green)
             else
-                printstyled(io, "Task failed. "; color=:red)
-                printstyled(io, "Captured exception: "; color=:light_black)
+                printstyled(io, "⨯ Task failed. "; color=:light_red)
+                printstyled(io, "Captured exception:\n"; color=:light_yellow)
                 printstyled(io, e_str == last_shown_exception[] ? "(same as previous)" : e_str)
                 last_shown_exception[] = e_str
                 println(io)
