003e727ddb4ecdeb18fb59296d1c36c48c06d975
Fons van der Plas <fonsvdplas@gmail.com>
2026-02-02 10:08:39 +0100

    Fix Julia 1.13 test stdlib

--- test/fixtures/this became a stdlib/test.jl
+++ test/fixtures/this became a stdlib/test.jl
@@ -8,12 +8,14 @@ proj, man = final_project_manifest_parsed(result)
 @test sort(collect(keys(proj["deps"]))) == ["Artifacts", "PlutoPkgTestA"]
 
 
-if VERSION > v"1.9.99999" || VERSION < v"1.6.9999"
+if VERSION < v"1.6.9999" || v"1.9.99999" < VERSION < v"1.12.999999"
     @test last(strats) == GracefulPkg.StrategyFixStdlibs()
     # there should be only one compat entry left
     @test proj["compat"] == Dict("PlutoPkgTestA" => "~0.2.2")
 else
     @test last(strats) == GracefulPkg.StrategyDoNothing()
-    # there should be only one compat entry left
+    # both compats should be left
     @test sort(collect(keys(proj["compat"]))) == ["Artifacts", "PlutoPkgTestA"]
+    
+    @show proj["compat"]["Artifacts"]
 end
